55e63bbcca50ebd550f73962baaf2f3de8dad4cc
ty-martz <jacksontmartz@gmail.com>
2022-01-09 16:54:35 -0500

    update csv name in main script

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,8 +1,14 @@
 from collector import get_christies_data
 
 def main():
-    scrape_data = get_christies_data()
-    scrape_data.to_csv('christies_data.csv')
+    yrs = [2010]
+    scrape_data = get_christies_data(years=yrs)
+    if len(yrs) == 1:
+        scrape_data.to_csv(f'data/christies_data_{yrs[0]}.csv')
+    else:
+        min_yr = min(yrs)
+        max_yr = max(yrs)
+        scrape_data.to_csv(f'data/christies_data_{min_yr}_{max_yr}.csv')
 
 if __name__ == '__main__':
     main()
