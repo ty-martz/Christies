405656b36c166ac0f0fa7762df0e208affc785f2
ty-martz <jacksontmartz@gmail.com>
2022-08-04 06:52:24 -0400

    add data export options

--- scripts/preprocessing/export_data_script.py
+++ scripts/preprocessing/export_data_script.py
@@ -1,14 +1,17 @@
 from clean_auction_data import *
 
-def main(url=True, algo=True):
+def main(url=True, csv=True, pickle=True):
     raw_data = pd.read_csv('data/christies_data.csv')
     df = clean_data(raw_data)
     if url:
         links = create_url_table(df)
         links.to_csv('data/auction_links.csv')
-    if algo:
+    if csv:
         mod = create_model_table(df)
         mod.to_csv('data/model_data.csv')
+    if pickle:
+        jar = create_model_table(df)
+        jar.to_pickle('data/model_data.pkl')
 
 if __name__ =='__main__':
-    main()
+    main(False, False, True)
